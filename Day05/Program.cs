e6ddd66d4b26caaf43be5c9e43787b0a04276468
Evan C <evanc5@users.noreply.github.com>
2022-12-04 23:59:53 -0500

    add and configure day 5

---
+++ Day05/Program.cs
@@ -0,0 +1,28 @@
+﻿Part1();
+Part2();
+
+static void Part1()
+{
+    var input = File.ReadAllLines(@".\input.txt");
+    var sw = System.Diagnostics.Stopwatch.StartNew();
+
+    var result = 0;
+
+    Console.WriteLine($"Part 1: {result}");
+
+    sw.Stop();
+    System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
+}
+
+static void Part2()
+{
+    var input = File.ReadAllLines(@".\input.txt");
+    var sw = System.Diagnostics.Stopwatch.StartNew();
+
+    var result = 0;
+
+    Console.WriteLine($"Part 2: {result}");
+
+    sw.Stop();
+    System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+}
