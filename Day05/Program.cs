97f18ab52596122aff5aa909c89686703f1574bd
Evan C <evanc5@users.noreply.github.com>
2022-12-05 11:38:15 -0500

    clean up day 5, automatically generate board

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -6,18 +6,13 @@ static void Part1()
     var input = File.ReadAllLines(@".\input.txt");
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
-    // var board = input.Take(8);
+    var board = input.Take(8);
     var moves = input.TakeLast(501);
-    var cargo = new Cargo();
+    var cargo = new Cargo(board);
 
-    // foreach (var line in board)
-    // {
-    //     //build board object
-    //     //idk how to do this actually so let's just do this manually for now I guess
-    // }
     foreach (var line in moves)
     {
-        cargo.ProcessMove1(line);
+        cargo.ProcessMove(line, 1);
     }
 
     Console.WriteLine($"Part 1: {cargo.GetTops()}");
@@ -31,21 +26,16 @@ static void Part2()
     var input = File.ReadAllLines(@".\input.txt");
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
-    // var board = input.Take(8);
+    var board = input.Take(8);
     var moves = input.TakeLast(501);
-    var cargo = new Cargo();
+    var cargo = new Cargo(board);
 
-    // foreach (var line in board)
-    // {
-    //     //build board object
-    //     //idk how to do this actually so let's just do this manually for now I guess
-    // }
     foreach (var line in moves)
     {
-        cargo.ProcessMove2(line);
+        cargo.ProcessMove(line, 2);
     }
 
-    Console.WriteLine($"Part 1: {cargo.GetTops()}");
+    Console.WriteLine($"Part 2: {cargo.GetTops()}");
 
     sw.Stop();
     System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
@@ -58,11 +48,6 @@ public class Cargo
     public Cargo()
     {
         Stacks = new Stack<char>[9];
-        InitializeStacks();
-    }
-
-    private void InitializeStacks()
-    {
         Stacks[0] = new Stack<char>();
         Stacks[1] = new Stack<char>();
         Stacks[2] = new Stack<char>();
@@ -72,81 +57,42 @@ public class Cargo
         Stacks[6] = new Stack<char>();
         Stacks[7] = new Stack<char>();
         Stacks[8] = new Stack<char>();
-
-        Stacks[0].Push('B');
-        Stacks[0].Push('S');
-        Stacks[0].Push('V');
-        Stacks[0].Push('Z');
-        Stacks[0].Push('G');
-        Stacks[0].Push('P');
-        Stacks[0].Push('W');
-
-        Stacks[1].Push('J');
-        Stacks[1].Push('V');
-        Stacks[1].Push('B');
-        Stacks[1].Push('C');
-        Stacks[1].Push('Z');
-        Stacks[1].Push('F');
-
-        Stacks[2].Push('V');
-        Stacks[2].Push('L');
-        Stacks[2].Push('M');
-        Stacks[2].Push('H');
-        Stacks[2].Push('N');
-        Stacks[2].Push('Z');
-        Stacks[2].Push('D');
-        Stacks[2].Push('C');
-
-        Stacks[3].Push('L');
-        Stacks[3].Push('D');
-        Stacks[3].Push('M');
-        Stacks[3].Push('Z');
-        Stacks[3].Push('P');
-        Stacks[3].Push('F');
-        Stacks[3].Push('J');
-        Stacks[3].Push('B');
-
-        Stacks[4].Push('V');
-        Stacks[4].Push('F');
-        Stacks[4].Push('C');
-        Stacks[4].Push('G');
-        Stacks[4].Push('J');
-        Stacks[4].Push('B');
-        Stacks[4].Push('Q');
-        Stacks[4].Push('H');
-
-        Stacks[5].Push('G');
-        Stacks[5].Push('F');
-        Stacks[5].Push('Q');
-        Stacks[5].Push('T');
-        Stacks[5].Push('S');
-        Stacks[5].Push('L');
-        Stacks[5].Push('B');
-
-        Stacks[6].Push('L');
-        Stacks[6].Push('G');
-        Stacks[6].Push('C');
-        Stacks[6].Push('Z');
-        Stacks[6].Push('V');
-
-        Stacks[7].Push('N');
-        Stacks[7].Push('L');
-        Stacks[7].Push('G');
-
-        Stacks[8].Push('J');
-        Stacks[8].Push('F');
-        Stacks[8].Push('H');
-        Stacks[8].Push('C');
     }
 
-    public void ProcessMove1(string move)
+    public Cargo(IEnumerable<string> board) : this()
+    {
+        var reversed = board.Reverse();
+        int[] columns = { 1, 5, 9, 13, 17, 21, 25, 29, 33 };
+        foreach (var row in reversed)
+        {
+            var currentStack = 0;
+            foreach (var i in columns)
+            {
+                if (char.IsLetter(row[i]))
+                {
+                    Stacks[currentStack].Push(row[i]);
+                }
+                currentStack++;
+            }
+        }
+    }
+
+    public void ProcessMove(string move, int version)
     {
         var split = move.Split(' ');
         var count = int.Parse(split[1]);
         var from = int.Parse(split[3]) - 1;
         var to = int.Parse(split[5]) - 1;
 
-        ProcessMove1(count, from, to);
+        switch (version)
+        {
+            case 1:
+                ProcessMove1(count, from, to);
+                break;
+            case 2:
+                ProcessMove2(count, from, to);
+                break;
+        }
     }
 
     public void ProcessMove1(int count, int from, int to)
@@ -156,16 +102,6 @@ public class Cargo
             Stacks[to].Push(Stacks[from].Pop());
             count--;
         }
-    }
-
-    public void ProcessMove2(string move)
-    {
-        var split = move.Split(' ');
-        var count = int.Parse(split[1]);
-        var from = int.Parse(split[3]) - 1;
-        var to = int.Parse(split[5]) - 1;
-
-        ProcessMove2(count, from, to);
     }
 
     public void ProcessMove2(int count, int from, int to)
