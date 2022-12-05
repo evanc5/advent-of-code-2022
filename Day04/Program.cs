dc36f0dcbe554727e136e10d04cd8b582ca9c18f
Evan C <evanc5@users.noreply.github.com>
2022-12-04 23:52:52 -0500

    complete day 4

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -7,7 +7,11 @@ static void Part1()
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
-
+    foreach (var line in input)
+    {
+        var split = line.Split(',');
+        if (FullyContains(split[0], split[1])) result++;
+    }
     Console.WriteLine($"Part 1: {result}");
 
     sw.Stop();
@@ -20,9 +24,38 @@ static void Part2()
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
-
+    foreach (var line in input)
+    {
+        var split = line.Split(',');
+        if (Overlaps(split[0], split[1])) result++;
+    }
     Console.WriteLine($"Part 2: {result}");
 
     sw.Stop();
     System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+}
+
+static bool FullyContains(string elf1, string elf2)
+{
+    var range1 = elf1.Split('-').Select(i => int.Parse(i)).ToArray();
+    var range2 = elf2.Split('-').Select(i => int.Parse(i)).ToArray();
+
+    if (range1[0] <= range2[0] && range1[1] >= range2[1]) return true;
+    if (range2[0] <= range1[0] && range2[1] >= range1[1]) return true;
+
+    return false;
+}
+
+static bool Overlaps(string elf1, string elf2)
+{
+    var range1 = elf1.Split('-').Select(i => int.Parse(i)).ToArray();
+    var range2 = elf2.Split('-').Select(i => int.Parse(i)).ToArray();
+
+    if (range1[0] <= range2[0] && range1[1] >= range2[1]) return true;
+    if (range2[0] <= range1[0] && range2[1] >= range1[1]) return true;
+
+    if (range1[0] <= range2[0] && range1[1] >= range2[0]) return true;
+    if (range2[0] <= range1[0] && range2[1] >= range1[0]) return true;
+
+    return false;
 }
