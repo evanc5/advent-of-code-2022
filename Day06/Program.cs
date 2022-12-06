eadec90ab96698803f4d3de66e9f71e040d9afa4
Evan C <evanc5@users.noreply.github.com>
2022-12-06 00:08:50 -0500

    complete day 6

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -3,13 +3,18 @@ Part2();
 
 static void Part1()
 {
-    var input = File.ReadAllLines(@".\input.txt");
+    var input = File.ReadAllText(@".\input.txt");
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
-    foreach (var line in input)
+    for (int i = 3; i < input.Length; i++)
     {
-
+        var lastFour = input.Substring(i - 3, 4);
+        if (lastFour.Distinct().Count() == 4)
+        {
+            result = i + 1;
+            break;
+        }
     }
     Console.WriteLine($"Part 1: {result}");
 
@@ -19,13 +24,18 @@ static void Part1()
 
 static void Part2()
 {
-    var input = File.ReadAllLines(@".\input.txt");
+    var input = File.ReadAllText(@".\input.txt");
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
-    foreach (var line in input)
+    for (int i = 13; i < input.Length; i++)
     {
-
+        var lastFourteen = input.Substring(i - 13, 14);
+        if (lastFourteen.Distinct().Count() == 14)
+        {
+            result = i + 1;
+            break;
+        }
     }
     Console.WriteLine($"Part 2: {result}");
 
