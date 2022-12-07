eb9d9a6647f7cd99aaf704ff75e6f99122859da7
Evan C <evanc5@users.noreply.github.com>
2022-12-07 00:58:39 -0500

    complete day 7 part 1

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -3,14 +3,33 @@ Part2();
 
 static void Part1()
 {
-    var input = File.ReadAllLines(@".\input.txt");
+    var input = File.ReadAllLines(@".\input.txt").Skip(1);
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
+    var fileSystem = new FileSystem();
     foreach (var line in input)
     {
-
+        var split = line.Split(' ');
+        switch (split[0])
+        {
+            case "$":
+                if (split[1] == "cd")
+                {
+                    fileSystem.cd(split[2]);
+                }
+                break;
+            case "dir":
+                var newSubdirectory = new Directory(split[1], fileSystem.CurrentDirectory);
+                fileSystem.CurrentSubdirectories.Add(newSubdirectory);
+                fileSystem.FlatDirectories.Add(newSubdirectory);
+                break;
+            default:
+                fileSystem.CurrentFiles.Add(split[1], int.Parse(split[0]));
+                break;
+        }
     }
+    result = fileSystem.FlatDirectories.Where(d => d.Size <= 100000).Sum(d => d.Size);
     Console.WriteLine($"Part 1: {result}");
 
     sw.Stop();
@@ -19,7 +38,7 @@ static void Part1()
 
 static void Part2()
 {
-    var input = File.ReadAllLines(@".\input.txt");
+    var input = File.ReadAllLines(@".\input.txt").Skip(1);
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
@@ -31,4 +50,82 @@ static void Part2()
 
     sw.Stop();
     System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+}
+
+class FileSystem
+{
+    public Directory CurrentDirectory { get; private set; }
+    public string CurrentPath => CurrentDirectory.Path;
+    public Dictionary<string, int> CurrentFiles => CurrentDirectory.Files;
+    public List<Directory> CurrentSubdirectories => CurrentDirectory.Subdirectories;
+    public IEnumerable<string> CurrentSubdirectoryNames => CurrentSubdirectories.Select(d => d.Path);
+    public Directory? ParentDirectory => CurrentDirectory.Parent;
+
+    public List<Directory> FlatDirectories { get; }
+
+    public FileSystem()
+    {
+        CurrentDirectory = new Directory("/");
+        FlatDirectories = new List<Directory>();
+    }
+
+    public void cd(string path)
+    {
+        if (path == "..")
+        {
+            if (ParentDirectory == null) throw new NullReferenceException(nameof(ParentDirectory));
+            CurrentDirectory = ParentDirectory;
+        }
+        else if (CurrentSubdirectoryNames.Contains(path))
+        {
+            CurrentDirectory = CurrentSubdirectories.First(d => d.Path == path);
+        }
+        else
+        {
+            CurrentDirectory = new Directory(path, CurrentDirectory);
+            FlatDirectories.Add(CurrentDirectory);
+        }
+    }
+
+    public void ls(IEnumerable<string> entries)
+    {
+
+    }
+}
+
+class Directory
+{
+    public string Path { get; }
+    public Dictionary<string, int> Files { get; }
+    public List<Directory> Subdirectories { get; }
+    public Directory? Parent { get; }
+
+    public int Size
+    {
+        get
+        {
+            var result = 0;
+            foreach (var fileSize in Files.Values)
+            {
+                result += fileSize;
+            }
+            foreach (var subdirectory in Subdirectories)
+            {
+                result += subdirectory.Size;
+            }
+            return result;
+        }
+    }
+
+    public Directory(string path)
+    {
+        Path = path;
+        Files = new Dictionary<string, int>();
+        Subdirectories = new List<Directory>();
+    }
+
+    public Directory(string path, Directory parent) : this(path)
+    {
+        Parent = parent;
+    }
 }
