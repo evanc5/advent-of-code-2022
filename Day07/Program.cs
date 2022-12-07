0388da75cd563abd54da7ce2930aed690c668eb1
Evan C <evanc5@users.noreply.github.com>
2022-12-07 01:10:15 -0500

    complete day 7 part 2

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -7,6 +7,30 @@ static void Part1()
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
+    var fileSystem = BuildFileSystem(input);
+    result = fileSystem.FlatDirectories.Where(d => d.Size <= 100000).Sum(d => d.Size);
+    Console.WriteLine($"Part 1: {result}");
+
+    sw.Stop();
+    System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
+}
+
+static void Part2()
+{
+    var input = File.ReadAllLines(@".\input.txt").Skip(1);
+    var sw = System.Diagnostics.Stopwatch.StartNew();
+
+    var result = 0;
+    var fileSystem = BuildFileSystem(input);
+    result = fileSystem.FlatDirectories.Where(d => d.Size >= fileSystem.SpaceToFree).Min(d => d.Size);
+    Console.WriteLine($"Part 2: {result}");
+
+    sw.Stop();
+    System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+}
+
+static FileSystem BuildFileSystem(IEnumerable<string> input)
+{
     var fileSystem = new FileSystem();
     foreach (var line in input)
     {
@@ -29,44 +53,29 @@ static void Part1()
                 break;
         }
     }
-    result = fileSystem.FlatDirectories.Where(d => d.Size <= 100000).Sum(d => d.Size);
-    Console.WriteLine($"Part 1: {result}");
-
-    sw.Stop();
-    System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
-}
-
-static void Part2()
-{
-    var input = File.ReadAllLines(@".\input.txt").Skip(1);
-    var sw = System.Diagnostics.Stopwatch.StartNew();
-
-    var result = 0;
-    foreach (var line in input)
-    {
-
-    }
-    Console.WriteLine($"Part 2: {result}");
-
-    sw.Stop();
-    System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+    return fileSystem;
 }
 
 class FileSystem
 {
     public Directory CurrentDirectory { get; private set; }
-    public string CurrentPath => CurrentDirectory.Path;
     public Dictionary<string, int> CurrentFiles => CurrentDirectory.Files;
     public List<Directory> CurrentSubdirectories => CurrentDirectory.Subdirectories;
     public IEnumerable<string> CurrentSubdirectoryNames => CurrentSubdirectories.Select(d => d.Path);
     public Directory? ParentDirectory => CurrentDirectory.Parent;
+
+    public int TotalSpace => 70000000;
+    public int RequiredSpace => 30000000;
+    public int UsedSpace => FlatDirectories[0].Size;
+    public int AvailableSpace => TotalSpace - UsedSpace;
+    public int SpaceToFree => RequiredSpace - AvailableSpace;
 
     public List<Directory> FlatDirectories { get; }
 
     public FileSystem()
     {
         CurrentDirectory = new Directory("/");
-        FlatDirectories = new List<Directory>();
+        FlatDirectories = new List<Directory> { CurrentDirectory };
     }
 
     public void cd(string path)
@@ -85,11 +94,6 @@ class FileSystem
             CurrentDirectory = new Directory(path, CurrentDirectory);
             FlatDirectories.Add(CurrentDirectory);
         }
-    }
-
-    public void ls(IEnumerable<string> entries)
-    {
-
     }
 }
 
