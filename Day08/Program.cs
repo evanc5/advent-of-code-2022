9cf0c9549a7f05ab4c871c36b8d75a9c49ff6da3
Evan C <evanc5@users.noreply.github.com>
2022-12-08 01:09:22 -0500

    add ForestExtensions

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -7,12 +7,12 @@ static void Part1()
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
     var result = 0;
-    var forest = BuildForest(input.ToArray());
+    var forest = ForestExtensions.BuildForest(input.ToArray());
     for (int x = 0; x < input.Count(); x++)
     {
         for (int y = 0; y < input[x].Length; y++)
         {
-            if (IsVisible(forest, x, y)) result++;
+            if (forest.IsVisible(x, y)) result++;
         }
     }
     Console.WriteLine($"Part 1: {result}");
@@ -26,13 +26,13 @@ static void Part2()
     var input = File.ReadAllLines(@".\input.txt");
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
-    var result = 0;
-    var forest = BuildForest(input.ToArray());
+    var result = int.MinValue;
+    var forest = ForestExtensions.BuildForest(input.ToArray());
     for (int x = 0; x < input.Count(); x++)
     {
         for (int y = 0; y < input[x].Length; y++)
         {
-            result = Math.Max(result, ScenicScore(forest, x, y));
+            result = Math.Max(result, forest.ScenicScore(x, y));
         }
     }
     Console.WriteLine($"Part 2: {result}");
@@ -41,110 +41,98 @@ static void Part2()
     System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
 }
 
-static int[,] BuildForest(string[] input)
+public static class ForestExtensions
 {
-    var forest = new int[input[0].Count(), input.Count()];
-    for (int i = 0; i < input.Count(); i++)
+    public static int[,] BuildForest(string[] input)
     {
-        for (int j = 0; j < input[i].Length; j++)
+        var forest = new int[input[0].Count(), input.Count()];
+        for (int i = 0; i < input.Count(); i++)
         {
-            forest[i, j] = int.Parse(input[i][j].ToString());
+            for (int j = 0; j < input[i].Length; j++)
+            {
+                forest[i, j] = int.Parse(input[i][j].ToString());
+            }
         }
-    }
-    return forest;
-}
-
-static bool IsVisible(int[,] forest, int x, int y)
-{
-    var rightEdge = forest.GetLength(0);
-    var bottomEdge = forest.GetLength(1);
-
-    if (x >= rightEdge || x == 0 || y >= bottomEdge || y == 0) return true;
-
-    var upVisible = true;
-    for (int i = y - 1; i >= 0; i--)
-    {
-        if (forest[x, i] >= forest[x, y])
-        {
-            upVisible = false;
-            break;
-        }
-    }
-    var downVisible = true;
-    for (int i = y + 1; i < bottomEdge; i++)
-    {
-        if (forest[x, i] >= forest[x, y])
-        {
-            downVisible = false;
-            break;
-        }
+        return forest;
     }
 
-    var leftVisible = true;
-    for (int i = x - 1; i >= 0; i--)
+    public static bool IsVisible(this int[,] forest, int x, int y)
     {
-        if (forest[i, y] >= forest[x, y])
+        var rightEdge = forest.GetLength(0);
+        var bottomEdge = forest.GetLength(1);
+
+        if (x >= rightEdge || x == 0 || y >= bottomEdge || y == 0) return true;
+
+        var upVisible = true;
+        for (int i = y - 1; i >= 0; i--)
         {
-            leftVisible = false;
-            break;
+            if (forest[x, i] >= forest[x, y])
+            {
+                upVisible = false;
+                break;
+            }
         }
-    }
-    var rightVisible = true;
-    for (int i = x + 1; i < rightEdge; i++)
-    {
-        if (forest[i, y] >= forest[x, y])
+        var downVisible = true;
+        for (int i = y + 1; i < bottomEdge; i++)
         {
-            rightVisible = false;
-            break;
+            if (forest[x, i] >= forest[x, y])
+            {
+                downVisible = false;
+                break;
+            }
         }
+        var leftVisible = true;
+        for (int i = x - 1; i >= 0; i--)
+        {
+            if (forest[i, y] >= forest[x, y])
+            {
+                leftVisible = false;
+                break;
+            }
+        }
+        var rightVisible = true;
+        for (int i = x + 1; i < rightEdge; i++)
+        {
+            if (forest[i, y] >= forest[x, y])
+            {
+                rightVisible = false;
+                break;
+            }
+        }
+        return upVisible || downVisible || leftVisible || rightVisible;
     }
 
-    return upVisible || downVisible || leftVisible || rightVisible;
-}
-
-static int ScenicScore(int[,] forest, int x, int y)
-{
-    var rightEdge = forest.GetLength(0);
-    var bottomEdge = forest.GetLength(1);
-
-    if (x >= rightEdge || x == 0 || y >= bottomEdge || y == 0) return 0;
-
-    var upVisible = 0;
-    for (int i = y - 1; i >= 0; i--)
+    public static int ScenicScore(this int[,] forest, int x, int y)
     {
-        upVisible++;
-        if (forest[x, i] >= forest[x, y])
-        {
-            break;
-        }
-    }
-    var downVisible = 0;
-    for (int i = y + 1; i < bottomEdge; i++)
-    {
-        downVisible++;
-        if (forest[x, i] >= forest[x, y])
-        {
-            break;
-        }
-    }
+        var rightEdge = forest.GetLength(0);
+        var bottomEdge = forest.GetLength(1);
 
-    var leftVisible = 0;
-    for (int i = x - 1; i >= 0; i--)
-    {
-        leftVisible++;
-        if (forest[i, y] >= forest[x, y])
+        if (x >= rightEdge || x == 0 || y >= bottomEdge || y == 0) return 0;
+
+        var upVisible = 0;
+        for (int i = y - 1; i >= 0; i--)
         {
-            break;
+            upVisible++;
+            if (forest[x, i] >= forest[x, y]) break;
         }
-    }
-    var rightVisible = 0;
-    for (int i = x + 1; i < rightEdge; i++)
-    {
-        rightVisible++;
-        if (forest[i, y] >= forest[x, y])
+        var downVisible = 0;
+        for (int i = y + 1; i < bottomEdge; i++)
         {
-            break;
+            downVisible++;
+            if (forest[x, i] >= forest[x, y]) break;
         }
+        var leftVisible = 0;
+        for (int i = x - 1; i >= 0; i--)
+        {
+            leftVisible++;
+            if (forest[i, y] >= forest[x, y]) break;
+        }
+        var rightVisible = 0;
+        for (int i = x + 1; i < rightEdge; i++)
+        {
+            rightVisible++;
+            if (forest[i, y] >= forest[x, y]) break;
+        }
+        return upVisible * downVisible * leftVisible * rightVisible;
     }
-    return upVisible * downVisible * leftVisible * rightVisible;
 }
