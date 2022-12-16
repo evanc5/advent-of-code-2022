bfb33b02892c8f289f51cff6be8c69b99e78d9cb
Evan C <evanc5@users.noreply.github.com>
2022-12-16 11:34:36 -0500

    complete part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,71 +1,72 @@
-﻿Part1();
-Part2();
+﻿var input = File.ReadAllLines(@".\input.txt");
+var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-static void Part1()
+var computer = new Computer();
+Console.WriteLine("Part 2:");
+foreach (var line in input)
 {
-    var input = File.ReadAllLines(@".\input.txt");
-    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
-
-    var computer = new Computer();
-    foreach (var line in input)
-    {
-        computer.Tick(line);
-    }
-    var result = computer.SignalStrengthHistory[20] +
-                 computer.SignalStrengthHistory[60] +
-                 computer.SignalStrengthHistory[100] +
-                 computer.SignalStrengthHistory[140] +
-                 computer.SignalStrengthHistory[180] +
-                 computer.SignalStrengthHistory[220];
-
-    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
-    Console.WriteLine($"Part 1: {result}");
-    System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
+    computer.Tick(line);
 }
+var part1 = computer.SignalStrengthHistory[20] +
+                computer.SignalStrengthHistory[60] +
+                computer.SignalStrengthHistory[100] +
+                computer.SignalStrengthHistory[140] +
+                computer.SignalStrengthHistory[180] +
+                computer.SignalStrengthHistory[220];
 
-static void Part2()
-{
-    var input = File.ReadAllLines(@".\input.txt");
-    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
-
-    var result = 0;
-    foreach (var line in input)
-    {
-
-    }
-
-    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
-    Console.WriteLine($"Part 2: {result}");
-    System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
-}
+var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+Console.WriteLine($"Part 1: {part1}");
+System.Diagnostics.Debug.WriteLine($"Part 1 + 2: {elapsedTime}");
 
 public class Computer
 {
-    public int CurrentCycle { get; private set; } = 1;
-    public int XRegister { get; private set; } = 1;
-    public int SignalStrength => CurrentCycle * XRegister;
-    public Dictionary<int, int> FutureAdds { get; } = new Dictionary<int, int>();
+    private int _currentCycle = 1;
+    private int _xRegister = 1;
+    private int _scanX;
+    private Dictionary<int, int> _futureAdds = new Dictionary<int, int>();
+    private int SignalStrength => _currentCycle * _xRegister;
+
     public Dictionary<int, int> SignalStrengthHistory { get; } = new Dictionary<int, int>();
 
-    public void Tick(string line = "")
+    public void Tick(string line)
     {
-        ProcessInstruction(line);
-        if (FutureAdds.TryGetValue(CurrentCycle, out var v)) XRegister += v;
-        SignalStrengthHistory[CurrentCycle] = SignalStrength;
+        var cycles = ProcessInstruction(line);
+        for (int i = 0; i < cycles; i++) Scan();
+        if (_futureAdds.TryGetValue(_currentCycle, out var v)) _xRegister += v;
+        SignalStrengthHistory[_currentCycle] = SignalStrength;
     }
 
-    public void ProcessInstruction(string instruction)
+    private void Scan()
+    {
+        if (Math.Abs(_xRegister - _scanX) <= 1)
+        {
+            Console.Write('#');
+        }
+        else
+        {
+            Console.Write(' ');
+        }
+        _scanX++;
+        if (_scanX > 39)
+        {
+            Console.WriteLine();
+            _scanX = 0;
+        }
+    }
+
+    private int ProcessInstruction(string instruction)
     {
         var split = instruction.Split(' ');
         switch (split[0])
         {
             case "addx":
-                CurrentCycle += 2;
-                FutureAdds[CurrentCycle] = int.Parse(split[1]);
-                break;
+                _currentCycle += 2;
+                _futureAdds[_currentCycle] = int.Parse(split[1]);
+                return 2;
             case "noop":
-                CurrentCycle++;
-                break;
+                _currentCycle++;
+                return 1;
         }
+        return 0;
     }
 }
