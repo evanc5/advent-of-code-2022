9d7c7c79c151afd8461e56b7edd8efdb12968221
Evan C <evanc5@users.noreply.github.com>
2022-12-18 00:35:12 -0500

    add and configure 17+18

---
+++ Day18/Program.cs
@@ -0,0 +1,33 @@
+﻿Part1();
+Part2();
+
+static void Part1()
+{
+    var input = File.ReadAllLines(@".\sample.txt");
+    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+
+    foreach (var line in input)
+    {
+
+    }
+    var result = 0;
+    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine($"Part 1: {result}");
+    System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
+}
+
+static void Part2()
+{
+    var input = File.ReadAllLines(@".\input.txt");
+    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+
+    var result = 0;
+    foreach (var line in input)
+    {
+
+    }
+
+    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine($"Part 2: {result}");
+    System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
+}
